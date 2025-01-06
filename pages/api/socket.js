6a823fcc69fca4749db8b6a84d3f628c7a74b227
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2025-01-06 17:36:21 +0530

    Update socket.js

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb+srv://priyanshu:110044@cluster0.pb8ps.mongodb.net/?ret
 
 
 // WebSocket server setup
-const wss = new WebSocket.Server({ port: 1000 });
+const wss = new WebSocket.Server({ port: 10000 });
 wss.on('connection', (ws) => {
     console.log('A new client connected');
     ws.send(JSON.stringify({ message: 'Welcome to the WebSocket server' }));
